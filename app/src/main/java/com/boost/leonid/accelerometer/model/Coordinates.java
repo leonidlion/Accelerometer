92f2fe746ab0cd78b5e5ef10531c2cb24b56da76
leonid <v666ad2009@gmail.com>
2016-12-16 18:04:55 +0200

    Add field for model

--- app/src/main/java/com/boost/leonid/accelerometer/model/Coordinates.java
+++ app/src/main/java/com/boost/leonid/accelerometer/model/Coordinates.java
@@ -1,6 +1,8 @@
 package com.boost.leonid.accelerometer.model;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Coordinates {
     private String date;
@@ -8,13 +10,18 @@ public class Coordinates {
     private List<Double> x;
     private List<Double> y;
     private List<Double> z;
+    private String model;
 
-    public Coordinates(String date, String time, List<Double> x, List<Double> y, List<Double> z) {
+    public Coordinates(){
+        // required for calls getValue(Coordinates.class)
+    }
+    public Coordinates(String date, String time, List<Double> x, List<Double> y, List<Double> z, String model) {
         this.date = date;
         this.time = time;
         this.x = x;
         this.y = y;
         this.z = z;
+        this.model = model;
     }
 
     public String getDate() {
@@ -55,5 +62,24 @@ public class Coordinates {
 
     public void setZ(List<Double> z) {
         this.z = z;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void SetModel(String deviceModel) {
+        this.model = deviceModel;
+    }
+
+    public Map<String, Object> toMap(){
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("date", date);
+        result.put("time", time);
+        result.put("x", x);
+        result.put("y", y);
+        result.put("z", z);
+        result.put("model", model);
+        return result;
     }
 }
