d051227c5b11dfe165ca12c9cba24d4e2198a155
leonid <v666ad2009@gmail.com>
2016-12-16 18:06:11 +0200

    Create view pager, tab layout and include fragments

--- app/src/main/java/com/boost/leonid/accelerometer/ui/MainActivity.java
+++ app/src/main/java/com/boost/leonid/accelerometer/ui/MainActivity.java
@@ -1,46 +1,89 @@
 package com.boost.leonid.accelerometer.ui;
 
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.boost.leonid.accelerometer.R;
+import com.boost.leonid.accelerometer.adapter.PagerAdapter;
 import com.boost.leonid.accelerometer.model.Coordinates;
-import com.google.firebase.auth.FirebaseAuth;
+import com.boost.leonid.accelerometer.ui.fragment.ListDatesFragment;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements ListDatesFragment.ClickItemListener{
     private static final String TAG = "MainActivity";
-    private FirebaseDatabase mFirebaseDatabase;
 
-    @BindView(R.id.hello)
-    TextView mHello;
+    private FirebaseDatabase mDatabase;
+    private FragmentPagerAdapter mPagerAdapter;
+    private static final int TAB_LIST_DATES = 0;
+    private static final int TAB_GRAPH      = 1;
+
+    /**
+     * Init views
+     */
+    @BindView(R.id.main_pager)
+    ViewPager mViewPager;
+    @BindView(R.id.toolbar_actionbar)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
 
-        ButterKnife.bind(this);
-        List<Double> list = new ArrayList<>();
-        Random random = new Random();
-        for (int i = 0; i < 10; i++){
-            list.add(random.nextDouble());
-        }
-        Coordinates coordinates = new Coordinates("13.12","14:00", list, list, list);
+        initToolbar();
+        mDatabase = FirebaseDatabase.getInstance();
+        mPagerAdapter = new PagerAdapter(getSupportFragmentManager(), getResources().getStringArray(R.array.tab_titles));
+        mViewPager = (ViewPager) findViewById(R.id.main_pager);
+        mViewPager.setAdapter(mPagerAdapter);
 
-        mFirebaseDatabase.getReference().child("users").child(getUid()).child("coord").setValue(coordinates);
-
-        mHello.setText("Hello " + FirebaseAuth.getInstance().getCurrentUser().getEmail());
-
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        tabLayout.setupWithViewPager(mViewPager);
 
     }
 
+    public void writeTestDataAcc(View view) {
+        List<Double> testList = new ArrayList<>();
+        for (int i = 0; i < 10; i++){
+            testList.add(new Random().nextDouble());
+        }
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+        String formatDate = sdf.format(date);
+        sdf = new SimpleDateFormat("hh:mm");
+        String formatTime = sdf.format(date);
+
+        String randomAccKey = mDatabase.getReference().child("users").child("acc_data").push().getKey();
+        Coordinates coordinates = new Coordinates(formatDate, formatTime, testList, testList, testList, getDeviceModel());
+
+        Map<String, Object> mapToInsert = new HashMap<>();
+        mapToInsert.put("/users/" + getUid() + "/acc_data/"  + randomAccKey, coordinates.toMap());
+
+        mDatabase.getReference().updateChildren(mapToInsert);
+
+    }
+
+    private void initToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public void onItemClick(String refKey) {
+        mViewPager.setCurrentItem(TAB_GRAPH, true);
+
+    }
 }
