39fe3c0db99595878a920d672fd84d066143ecdc
leonid <v666ad2009@gmail.com>
2016-12-20 19:03:57 +0200

    Refactor constructor, add method to update graph fragment

--- app/src/main/java/com/boost/leonid/accelerometer/adapter/PagerAdapter.java
+++ app/src/main/java/com/boost/leonid/accelerometer/adapter/PagerAdapter.java
@@ -1,34 +1,61 @@
 package com.boost.leonid.accelerometer.adapter;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-
+import android.util.Log;
 
 import com.boost.leonid.accelerometer.ui.fragment.GraphFragment;
 import com.boost.leonid.accelerometer.ui.fragment.ListDatesFragment;
 
 public class PagerAdapter extends FragmentPagerAdapter {
-    private final Fragment[] mFragments = new Fragment[]{
-            new ListDatesFragment(),
-            new GraphFragment()
-    };
+    private static final String TAG         = "PagerAdapter";
+    private static final int FRAGMENT_LIST  = 0;
+    private static final int FRAGMENT_GRAPH = 1;
+
+    private Bundle mBundle;
+
     private final String[] mFragmentTitles;
 
-
-    public PagerAdapter(FragmentManager fm, String[] stringArray) {
+    public PagerAdapter(FragmentManager fm, String[] titles, Bundle args){
         super(fm);
-        mFragmentTitles = stringArray;
+        Log.d("ADAPTER", "CREATE");
+        mFragmentTitles = titles;
+        mBundle = args;
+    }
+
+    public void updateGraph(Bundle args){
+        Log.d("ADAPTER", "updateGraph");
+        mBundle = args;
+        getItem(FRAGMENT_GRAPH);
+    }
+    @Override
+    public int getItemPosition(Object object) {
+        return POSITION_NONE;
     }
 
     @Override
     public Fragment getItem(int position) {
-        return mFragments[position];
+        Log.d("ADAPTER", "getItem");
+        Fragment fragment = null;
+        switch (position){
+            case FRAGMENT_LIST:
+                Log.d(TAG, "fragment list");
+                fragment = new ListDatesFragment();
+                break;
+            case FRAGMENT_GRAPH:
+                Log.d(TAG, "graph");
+                fragment = new GraphFragment();
+                fragment.setArguments(mBundle);
+                break;
+        }
+        return fragment;
     }
 
     @Override
     public int getCount() {
-        return mFragments.length;
+        return mFragmentTitles.length;
     }
 
     @Override
