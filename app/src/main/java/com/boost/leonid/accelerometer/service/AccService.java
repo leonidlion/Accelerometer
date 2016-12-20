c170b1b0f40255a96661e7b38418d32341437a73
leonid <v666ad2009@gmail.com>
2016-12-20 18:57:55 +0200

    Add handler for delay before save data to server (test 5 sec)

--- app/src/main/java/com/boost/leonid/accelerometer/service/AccService.java
+++ app/src/main/java/com/boost/leonid/accelerometer/service/AccService.java
@@ -8,7 +8,10 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
@@ -16,10 +19,8 @@ import com.boost.leonid.accelerometer.model.Coordinates;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -29,34 +30,30 @@ public class AccService extends Service implements SensorEventListener{
 
     public static final String EXTRA_USER_ID = "user_id";
 
+    private static final int SAVE_DELAY_MS = 5000;
+    private static final String EXTRA_MSG_ARRAY = "ARRAY";
+
     private FirebaseDatabase mDatabase;
     private SensorManager mSensorManager;
     private String mUser_id;
-    private String mStartDate, mStartTime;
+    private String mStartDate, mStartTime, mEndTime;
     private String mStartKey;
     private Map<String, Object> mapToInsert = new HashMap<>();
     private Coordinates mCoordinates;
-    private Map<String, Object> map = new HashMap<>();
-    private List<Float> x = new ArrayList<>();
-    private List<Float> y = new ArrayList<>();
-    private List<Float> z = new ArrayList<>();
+    private Handler mHandler;
+    private Bundle mBundle;
 
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(TAG, "onStartCommand");
-
         initStartDateTime();
-
         mUser_id = intent.getStringExtra(EXTRA_USER_ID);
         mDatabase = FirebaseDatabase.getInstance();
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         Sensor sensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
         mStartKey = mDatabase.getReference().child("users").child(mUser_id).child("acc_data").push().getKey();
-
         mCoordinates = new Coordinates(mStartDate, mStartTime, Build.MODEL);
-
         mSensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
         return super.onStartCommand(intent, flags, startId);
     }
@@ -66,7 +63,7 @@ public class AccService extends Service implements SensorEventListener{
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
         mStartDate = dateFormat.format(date);
 
-        dateFormat = new SimpleDateFormat("hh:mm");
+        dateFormat = new SimpleDateFormat("hh:mm:ss", Locale.getDefault());
         mStartTime = dateFormat.format(date);
     }
 
@@ -75,13 +72,36 @@ public class AccService extends Service implements SensorEventListener{
     public void onCreate() {
         super.onCreate();
         Log.d(TAG, "onCreate");
+        mBundle = new Bundle();
+
+        mHandler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                switch (msg.what){
+                    case 0:
+                        Log.d(TAG, "handleMessage");
+
+                        mCoordinates.addCoord(msg.getData().getFloatArray(EXTRA_MSG_ARRAY));
+                        mapToInsert.clear();
+                        mapToInsert.put("/users/" + mUser_id + "/acc_data/" + mStartKey, mCoordinates.allToMap());
+                        mDatabase.getReference().updateChildren(mapToInsert);
+
+                        this.removeCallbacksAndMessages(null);
+                        break;
+                }
+            }
+        };
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy");
+        mEndTime = new SimpleDateFormat("hh:mm:ss", Locale.getDefault()).format(new Date());
         mSensorManager.unregisterListener(this);
+        mHandler.removeCallbacksAndMessages(null);
+        mDatabase.getReference().child("users").child(mUser_id).child("acc_data").child(mStartKey).child("endTime").setValue(mEndTime);
     }
 
     @Nullable
@@ -92,14 +112,12 @@ public class AccService extends Service implements SensorEventListener{
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        Log.d(TAG, "onSensorChanged");
-
-        mCoordinates.addCoord(sensorEvent.values);
-        mapToInsert.clear();
-        mapToInsert.put("/users/" + mUser_id + "/acc_data/" + mStartKey, mCoordinates.allToMap());
-        mDatabase.getReference().updateChildren(mapToInsert);
+        mBundle.clear();
+        mBundle.putFloatArray(EXTRA_MSG_ARRAY, sensorEvent.values);
+        Message msg = new Message();
+        msg.setData(mBundle);
+        mHandler.sendMessageDelayed(msg, SAVE_DELAY_MS);
     }
-
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
