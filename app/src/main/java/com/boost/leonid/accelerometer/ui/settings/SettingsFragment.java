2b0ab36c56159d5e72123519c544a779058531f1
leonid <v666ad2009@gmail.com>
2016-12-22 19:18:50 +0200

    Add init summary

--- app/src/main/java/com/boost/leonid/accelerometer/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/boost/leonid/accelerometer/ui/settings/SettingsFragment.java
@@ -10,19 +10,37 @@ import com.boost.leonid.accelerometer.R;
 
 public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener{
     public static final String KEY_PREF_INTERVAL = "pref_list_interval";
+    public static final String KEY_PREF_TIME_WHEN_START = "pref_time_start";
+    public static final String KEY_PREF_TIME_DURATION = "pref_task_duration";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        initSummary();
     }
 
+    private void initSummary() {
+        Preference intervalPreference = findPreference(KEY_PREF_INTERVAL);
+        intervalPreference.setSummary(getPreferenceManager().getSharedPreferences().getString(KEY_PREF_INTERVAL, ""));
+
+        intervalPreference = findPreference(KEY_PREF_TIME_DURATION);
+        intervalPreference.setSummary(getPreferenceManager().getSharedPreferences().getString(KEY_PREF_TIME_DURATION, ""));
+    }
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
-        if (s.equals(KEY_PREF_INTERVAL)){
-            Preference intervalPreference = findPreference(s);
-            intervalPreference.setSummary(sharedPreferences.getString(s, ""));
+        Preference intervalPreference;
+        switch (s){
+            case KEY_PREF_INTERVAL:
+                intervalPreference = findPreference(s);
+                intervalPreference.setSummary(sharedPreferences.getString(s, ""));
+                break;
+            case KEY_PREF_TIME_DURATION:
+                intervalPreference = findPreference(s);
+                intervalPreference.setSummary(sharedPreferences.getString(s, ""));
+                break;
         }
     }
 
